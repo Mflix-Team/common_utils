d0bcd05a0a0fbb35c33c557da8a4e1a468bbd987
A <kingiswinter@gmail.com>
2025-03-27 21:15:33 +0800

    feat: 添加常用工具类
    - 新增多个工具类，包括日期、加密、日志、JSON、货币、数字和对象操作
    - 添加项目基础文件，如.gitignore、CHANGELOG.md、README.md等
    - 引入必要的依赖包

---
+++ lib/hex_color.dart
@@ -0,0 +1,39 @@
+/*
+ * @Author: A kingiswinter@gmail.com
+ * @Date: 2024-12-03 21:04:16
+ * @LastEditors: A kingiswinter@gmail.com
+ * @LastEditTime: 2024-12-20 20:17:06
+ * @FilePath: /mobile/package/common_utils/lib/hex_color.dart
+ * 
+ * Copyright (c) 2024 by A kingiswinter@gmail.com, All Rights Reserved.
+ */
+import 'dart:ui';
+
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF$hexColor';
+    }
+
+    final hexNum = int.parse(hexColor, radix: 16);
+
+    if (hexNum == 0) {
+      return 0xff000000;
+    }
+    return hexNum;
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+}
+
+extension ColorToHex on Color {
+  ///convert material colors to hexcolor
+  String toHexString() {
+    final alpha = (a * 255).toInt().toRadixString(16).padLeft(2, '0');
+    final red = (r * 255).toInt().toRadixString(16).padLeft(2, '0');
+    final green = (g * 255).toInt().toRadixString(16).padLeft(2, '0');
+    final blue = (b * 255).toInt().toRadixString(16).padLeft(2, '0');
+    return '#$red$green$blue$alpha'.toUpperCase();
+  }
+}
---
+++ lib/text_util.dart
@@ -0,0 +1,105 @@
+/*
+ * @Author: A kingiswinter@gmail.com
+ * @Date: 2024-12-03 21:04:16
+ * @LastEditors: A kingiswinter@gmail.com
+ * @LastEditTime: 2025-01-21 19:26:48
+ * @FilePath: /mobile/package/common_utils/lib/text_util.dart
+ * 
+ * Copyright (c) 2025 by A kingiswinter@gmail.com, All Rights Reserved.
+ */
+
+import 'dart:math';
+
+/// Text Util.
+class TextUtil {
+  /// isEmpty
+  static bool isEmpty(String? text) {
+    return text == null || text.isEmpty;
+  }
+
+  /// 每隔 x位 加 pattern
+  static String formatDigitPattern(String text,
+      {int digit = 4, String pattern = ' '}) {
+    text = text.replaceAllMapped(RegExp('(.{$digit})'), (Match match) {
+      return '${match.group(0)}$pattern';
+    });
+    if (text.endsWith(pattern)) {
+      text = text.substring(0, text.length - 1);
+    }
+    return text;
+  }
+
+  /// 每隔 x位 加 pattern, 从末尾开始
+  static String formatDigitPatternEnd(String text,
+      {int digit = 4, String pattern = ' '}) {
+    String temp = reverse(text);
+    temp = formatDigitPattern(temp, digit: digit, pattern: pattern);
+    temp = reverse(temp);
+    return temp;
+  }
+
+  /// 每隔4位加空格
+  static String formatSpace4(String text) {
+    return formatDigitPattern(text);
+  }
+
+  /// 每隔3三位加逗号
+  /// num 数字或数字字符串。int型。
+  static String formatComma3(Object num) {
+    return formatDigitPatternEnd(num.toString(), digit: 3, pattern: ',');
+  }
+
+  /// 每隔3三位加逗号
+  /// num 数字或数字字符串。double型。
+  static String formatDoubleComma3(Object num,
+      {int digit = 3, String pattern = ','}) {
+    List<String> list = num.toString().split('.');
+    String left =
+        formatDigitPatternEnd(list[0], digit: digit, pattern: pattern);
+    String right = list[1];
+    return '$left.$right';
+  }
+
+  /// hideNumber
+  static String hideNumber(String phoneNo,
+      {int start = 3, int end = 7, String replacement = '****'}) {
+    return phoneNo.replaceRange(start, end, replacement);
+  }
+
+  /// replace
+  static String replace(String text, Pattern from, String replace) {
+    return text.replaceAll(from, replace);
+  }
+
+  /// split
+  static List<String> split(String text, Pattern pattern) {
+    return text.split(pattern);
+  }
+
+  /// reverse
+  static String reverse(String text) {
+    if (isEmpty(text)) return '';
+    StringBuffer sb = StringBuffer();
+    for (int i = text.length - 1; i >= 0; i--) {
+      sb.writeCharCode(text.codeUnitAt(i));
+    }
+    return sb.toString();
+  }
+
+  /// durationFormat
+  static String durationFormat(Duration duration) {
+    if (duration.inSeconds == 0) return '00:00:00';
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return '${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds';
+  }
+
+  /// 格式化字节大小
+  static String formatBytes(int bytes, {int decimals = 2}) {
+    if (bytes <= 0) return '0 B';
+    const suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    var i = (log(bytes) / log(1024)).floor();
+    return '${(bytes / pow(1024, i)).toStringAsFixed(decimals)} ${suffixes[i]}';
+  }
+}
